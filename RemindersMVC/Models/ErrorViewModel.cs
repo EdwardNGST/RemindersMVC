25405d2ed6470a6d5d280fd7b2a58395fc43ecbc
alan_ <alan_hernandezgarcia@hotmail.com>
2019-01-11 16:30:55 -0600

    Agregar archivos de proyecto.

---
+++ RemindersMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RemindersMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
